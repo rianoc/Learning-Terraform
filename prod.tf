fed08396647a666d71eb87dc8bc35b4ab881dd6d
Rian Ó Cuinneagáin <rocuinneagain@firstderivatives.com>
2022-01-31 19:47:00 +0000

    Use variable for egress cidr

--- prod.tf
+++ prod.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
 
   tags = {
