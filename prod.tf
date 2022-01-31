31c36cdcef16ec9cb090b72d291d8387973a3086
Rian Ó Cuinneagáin <rocuinneagain@firstderivatives.com>
2022-01-31 15:43:50 +0000

    Add load balancer

--- prod.tf
+++ prod.tf
@@ -10,6 +10,20 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone= "eu-west-1a"
+    tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone= "eu-west-1b"
+    tags = {
+    "Terraform" : "true"
+  }
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "Allow http/https inbound. Everything outbound"
@@ -59,6 +73,24 @@ resource "aws_eip_association" "prod_web" {
 }
 
 resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  
   tags = {
     "Terraform" : "true"
   }
